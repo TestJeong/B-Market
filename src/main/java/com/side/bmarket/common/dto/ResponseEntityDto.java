9966755f46290cbc145faaffc3ad11b43ad4928d
Jeong <jyunj3015@gmail.com>
2023-12-09 16:25:07 +0900

    Add : ResponseEntityDto 추가

---
+++ src/main/java/com/side/bmarket/common/dto/ResponseEntityDto.java
@@ -0,0 +1,10 @@
+package com.side.bmarket.common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ResponseEntityDto {
+    private String message;
+}
