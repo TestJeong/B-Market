c0277e97d28f5d238bf91623b8adec9dc2618e42
Jeong <jyunj3015@gmail.com>
2023-12-11 22:36:40 +0900

    Modify : 공통 ResponseEntityDto 형식 수정

--- src/main/java/com/side/bmarket/common/dto/ResponseEntityDto.java
+++ src/main/java/com/side/bmarket/common/dto/ResponseEntityDto.java
@@ -1,10 +1,24 @@
 package com.side.bmarket.common.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
-@AllArgsConstructor
-public class ResponseEntityDto {
+public class ResponseEntityDto<T> {
+    private int code;
+    private HttpStatus status;
     private String message;
+    private T data;
+
+    public ResponseEntityDto(HttpStatus status, String message, T data) {
+        this.code = status.value();
+        this.status = status;
+        this.message = message;
+        this.data = data;
+    }
+
+    public static <T> ResponseEntityDto<T> of(HttpStatus httpStatus, T data) {
+        return new ResponseEntityDto<>(httpStatus, httpStatus.name(), data);
+    }
+
 }
